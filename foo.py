ffe7eb256cd56e6136b9b71e64deeaeceb5c5c2f
brodie2 <junk@dackz.net>
2015-12-10 14:03:56 -0800

    Create foo.py

---
+++ foo.py
@@ -0,0 +1,46 @@
+"""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor mauris
+vel eros feugiat vulputate. Nunc suscipit dictum tortor vitae consequat. Nunc
+vel odio lorem. Phasellus sapien sem, tempus vel facilisis et, bibendum ut
+dolor. Sed ultricies egestas dapibus. Nunc ut tempor diam, vitae iaculis eros.
+In hac habitasse platea dictumst. Sed sit amet nibh vel enim placerat aliquet.
+
+Etiam et finibus ipsum. Etiam tincidunt pellentesque vulputate. Vestibulum
+molestie urna est, vel ultricies odio ullamcorper sit amet. Duis ultrices sem
+libero, semper posuere lectus mattis et. Donec gravida nibh a augue imperdiet
+vehicula. Nulla efficitur faucibus dui, condimentum vehicula nisi interdum
+vel. Mauris eleifend egestas ipsum eu facilisis. Nulla facilisi. Mauris eget
+elit a ex commodo suscipit.
+
+Donec molestie est a felis molestie pellentesque. Donec tincidunt ullamcorper
+nulla et pharetra. Cum sociis natoque penatibus et magnis dis parturient
+montes, nascetur ridiculus mus. Suspendisse risus mi, molestie ut leo pretium,
+faucibus vestibulum metus. Phasellus congue eu sem et convallis. Nam
+consectetur est nisl, at lacinia magna scelerisque quis. Nam scelerisque quis
+ex eget malesuada.
+
+Suspendisse sed bibendum tellus, et congue mi. Donec efficitur dolor at
+lobortis mollis. Praesent in ultrices orci, vitae pharetra diam. Proin vel
+ultricies risus. Nullam nisi metus, condimentum at sapien quis, sagittis
+pharetra tellus. Pellentesque arcu turpis, dictum quis diam eu, feugiat
+egestas turpis. Nullam at enim ultricies, lobortis lacus quis, pulvinar lacus.
+Quisque vestibulum, orci vel commodo tempor, justo sapien euismod lorem, in
+egestas libero erat vitae sapien. Suspendisse egestas purus id odio porta, id
+convallis velit bibendum. Donec euismod leo id elit viverra rhoncus. Aliquam
+aliquam, mauris quis placerat ullamcorper, erat felis elementum nisi, id
+tristique ipsum mauris non risus. Integer bibendum non libero eget pharetra.
+Aliquam erat volutpat. Cras ut blandit orci. Sed volutpat nibh a est auctor
+vestibulum. Cras rhoncus maximus mauris sit amet finibus.
+
+Aliquam ut purus nec neque faucibus ultricies quis vitae urna. Donec pretium
+justo arcu, eget varius augue iaculis eu. Nam nec velit quis diam vestibulum
+ullamcorper. Curabitur auctor arcu quis leo ornare, pulvinar molestie lacus
+maximus. Praesent ultricies leo sit amet urna gravida, vitae iaculis dolor
+imperdiet. Donec at molestie odio. Aenean laoreet porta tellus et pulvinar.
+Integer non posuere lacus. Morbi vitae eros ut sapien vehicula venenatis non
+nec arcu. Vivamus sed purus vitae mauris commodo suscipit. Duis porta eros
+libero, nec posuere leo feugiat at. Quisque sodales orci non purus ultrices,
+a egestas libero vulputate. Pellentesque tincidunt libero eget nunc cursus,
+quis aliquet sem dignissim. Vestibulum ante ipsum primis in faucibus orci
+luctus et ultrices posuere cubilia Curae; Praesent ultricies fringilla ipsum
+et dapibus.
+"""
